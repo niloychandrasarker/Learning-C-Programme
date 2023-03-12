767425b48d76da11283bea873d4176974c3a0563
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-13 00:58:29 +0600

    arithmatic oparator

---
+++ 4_arithmatic_oparator.cpp
@@ -0,0 +1,28 @@
+
+// arithmatics oparator 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum,sub,mul,rem;
+   
+    cout<< "numner1= " ;
+      cin>>num1;
+    cout<< "numner2= " ;
+    cin>>num2;
+    sum=num1+num2;
+    cout<< "sum=" << sum <<endl;
+
+    sub=num1-num2;
+    cout<< "sub=" << sub <<endl;
+
+     mul=num1*num2;
+    cout<< "mul=" << mul <<endl;
+
+     rem=num1/num2;
+    cout<< "rem=" << rem;
+
+
+
+}
