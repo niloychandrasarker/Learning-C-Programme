fb27254917e5e1dde0e74fa16a760bb9a95bb125
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-13 00:46:41 +0600

    sum

---
+++ 3_sum.cpp
@@ -0,0 +1,17 @@
+
+// sum 2 number 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2,sum;
+   
+    cout<< "numner1= " ;
+      cin>>num1;
+    cout<< "numner2= " ;
+    cin>>num2;
+    sum=num1+num2;
+    cout<< "sum=" << sum;
+
+}
