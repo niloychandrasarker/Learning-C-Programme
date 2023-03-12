7f6d456610d5721a9689efc68ad88742ce0da9c2
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-13 01:29:05 +0600

    area of triangle

---
+++ 5_area_of_triangle.cpp
@@ -0,0 +1,17 @@
+
+// sum 2 number 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int base,height,area;
+   
+    cout<< "Enter Base= " ;
+      cin>>base;
+    cout<< "Enter Hight= " ;
+    cin>>height;
+    area = 0.5*base*height;
+    cout<< "Area of triangle=" << area;
+
+}
