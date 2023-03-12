0a16df98277b3a8738be31fe8c14aa7587396b9e
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-12 14:34:01 +0600

    first commit

---
+++ 1_datatype.cpp
@@ -0,0 +1,16 @@
+
+// datatype in C++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1=10,num2=20;
+    double num3=10.34345,num4=20.3243;
+    char Ch='a';
+    
+    cout<< "numner1= "<< num1<< endl << "number1= " <<  num2 << endl ;
+    cout<< "numner3= "<< num3<< endl << "number4= " <<  num4 << endl ;
+    cout<< "Charascter=" << Ch;
+
+}
