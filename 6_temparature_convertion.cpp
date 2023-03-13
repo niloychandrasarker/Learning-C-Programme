9493df7dc811f5bdae98349e741dbfcd0a4a9cd1
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-13 12:17:37 +0600

    temarature convertion

---
+++ 6_temparature_convertion.cpp
@@ -0,0 +1,19 @@
+
+// sum 2 number 
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  double faren,cels,kel;
+   
+    cout<< "Enter celcius temparature= " ;
+      cin>>cels;
+   faren=1.8*cels+32;
+    cout<< "fahrenheit= "<< faren <<endl ;
+    cout<< "Enter fahrenheit temparature= " ;
+      cin>>faren;
+      cels=(faren-32)/1.8;
+    cout<< "celcius=" << cels;
+
+}
