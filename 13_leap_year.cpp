787b9472ae314a1206c0283e592310ce5604463b
Niloy Sarker <niloysarker.cs@gmail.com>
2023-03-13 14:03:26 +0600

    leap year

---
+++ 13_leap_year.cpp
@@ -0,0 +1,17 @@
+//C++ program to check if a given year is a leap year or not
+#include<iostream>
+using namespace std;
+int main()
+{
+    int yr;
+    cout<<"Enter the Year: ";
+    cin>>yr;
+    if((yr%4==0) && (yr%100!=0))
+        cout<<"\nIt is a Leap Year";
+    else if(yr%400==0)
+        cout<<"\nIt is a Leap Year";
+    else
+        cout<<"\nIt is not a Leap Year";
+    cout<<endl;
+    return 0;
+}
